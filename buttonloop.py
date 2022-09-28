4273492ec06d222b44963f93afb2143cc277a3eb
Mark Krutulis <mark.krutulis@gmail.com>
2022-09-28 15:56:16 -0700

    Printed axis values

--- buttonloop.py
+++ buttonloop.py
@@ -1,4 +1,5 @@
 import pygame
+import numpy as np
 
 pygame.init()
 #pygame.joystick.init()
@@ -11,15 +12,26 @@ print(j.get_name())
 
 # Event loop demo
 
+flag = 0
+axes = np.zeros(4)
+
 try:
     while True:
         events = pygame.event.get()
+        
         for event in events:
             if event.type == pygame.JOYBUTTONDOWN:
                 b = event.button
                 print(f'{b}Button Pressed')
             elif event.type == pygame.JOYBUTTONUP:
                 print('Button Released')
+            elif event.type == pygame.JOYAXISMOTION:
+                axes[event.axis] = event.value
+        
+        flag += 1
+        if flag % 200000 == 0:
+            print(axes)
+
 
 except KeyboardInterrupt:
     print("Goodbye")
