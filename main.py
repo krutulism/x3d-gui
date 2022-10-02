ae89de0fdb5a3c4692d9e7b0552b78b4049a5eb6
markkrutulis <mark.krutulis@gmail.com>
2022-10-01 21:37:21 -0700

    Cleaned up main skeleton, drew on surface

--- main.py
+++ main.py
@@ -2,31 +2,39 @@ import pygame
 import sys
 import numpy as np
 
-pygame.init()
+def main():
 
-screen = pygame.display.set_mode([500, 500])
+    pygame.init()
 
-running = True
+    surface = pygame.display.set_mode([500, 500])
 
-
-while running
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
-
-    
+    running = True
 
 
+    rect = pygame.Rect(20,40,60,80)
+    pygame.draw.rect(surface, (0,127,127),rect)
 
-    pygame.display.flip()
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
+            
+
+        
+
+
+
+        pygame.display.flip()
 
 
 
 class Panel(pygame.Rect):
     def __init__(self, dimensions, position, label, value):
+        super().__init__()
         self.pos = position
         self.dims = dimensions
         self.label = label
         self.value = value
-    
-    
+        
 
+if __name__=='__main__':
+    main()
