3079ba3decb3c203108c907b01cd1fb2e68cf6bb
markkrutulis <mark.krutulis@gmail.com>
2022-10-20 17:45:35 -0700

    Added joystick init

--- main.py
+++ main.py
@@ -6,49 +6,37 @@ def main():
 
     pygame.init()
 
+    joysticks = [pygame.joystick.Joystick(i) for i in range(pygame.joystick.get_count())]
+    j = joysticks[0]
+    j.init()
+    print(j.get_name())
+
     W = 800
     H = 600
-
     surface = pygame.display.set_mode([W, H])
     pygame.display.set_caption("X3D GUI")
-
     myfont = pygame.freetype.SysFont('Helvetica', 11)
-    text = "Hello World"
-    textSize = 11
-
-    textRect = myfont.get_rect(text, size = textSize)
-    textRect.center = (W // 2, H // 2)
-
-    myfont.render_to(surface, textRect, text, (0,0,255), size=textSize)
-
-
 
     running = True
-    # rect1 = pygame.Rect(20,40,60,80)
-    # rect2 = pygame.Rect(120,140,60,80)
 
-    # pygame.draw.rect(surface, (0,127,127),rect1)
-    # pygame.draw.rect(surface, (127,127,0), rect2)
-
-    drawPanes(surface)
+    btnPanes, axPanes, hatPanes = drawPanes(surface, myfont)
 
     pygame.display.update()
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT: sys.exit()
-            elif event.type == pygame.KEYDOWN: pass
-                # if event.key == pygame.K_g:
-                #     pygame.draw.rect(surface, (255,0,0), rect1)
-                #     pygame.display.update(rect1)
-                #     pygame.draw.rect(surface, (255,0,0), rect2)
-                #     pygame.display.update(rect2)
-            elif event.type == pygame.KEYUP: pass
-                # if event.key == pygame.K_g:
-                #     pygame.draw.rect(surface, (0,127,127), rect1)
-                #     pygame.display.update(rect1)
-                #     pygame.draw.rect(surface, (127,127,0), rect2)
-                #     pygame.display.update(rect2)
+            elif event.type == pygame.JOYBUTTONDOWN:
+                b = event.button
+                for i in range(0,12):
+                    if b == i:
+                        print(f'Button {i} depressed')
+
+            elif event.type == pygame.JOYAXISMOTION:pass
+
+
+            elif event.type == pygame.JOYHATMOTION:pass
+
 
 def drawPanes(surface, font):
     #Coordinates to help draw grid of indicators
@@ -62,11 +50,16 @@ def drawPanes(surface, font):
     vHat = [530, 615, 700]
     hatDims = (60, 60)
 
+    textSize = 14
+
     btnPanes = []
     for i in range(0,len(hBtn)):
         rect = pygame.Rect(vBtn, hBtn[i], btnDims[0], btnDims[1])
         btnPanes.append(rect)
         pygame.draw.rect(surface, (127,127,0), rect)
+
+        text = f'Button  {i+1}'
+        drawLabel(surface, font, rect.center,  text, textSize)
 
     axPanes = []
     for i in range(0,len(hAx)):
@@ -74,12 +67,28 @@ def drawPanes(surface, font):
         axPanes.append(rect)
         pygame.draw.rect(surface, (127,127,0), rect)
 
+        text = f'Axis {i+1}'
+        rectcenter = rect.center
+        center = (rectcenter[0],rectcenter[1]-30) #Label the top of the pane  
+        drawLabel(surface, font, center, text, textSize)
+
     hatPanes = []
     for i in range(0,3):
         for j in range(0,3):
             rect = pygame.Rect(vHat[i], hHat[j], hatDims[0], hatDims[1])
             hatPanes.append(rect)
             pygame.draw.rect(surface, (0, 127, 127), rect)
+
+            text = f'{i-1},{-(j-1)}'  #Label hat 'axes' in order and by correct value
+            drawLabel(surface, font, rect.center, text, textSize)
+
+    return btnPanes, axPanes, hatPanes
+
+
+def drawLabel(surface, font, center, text, textSize):
+    textRect = font.get_rect(text, size=textSize)
+    textRect.center = center
+    font.render_to(surface, textRect, text, (0,0,255), size=textSize)
 
 
 if __name__=='__main__':
